6ab27ebe7ee148c405f55336e1477d190d9c98f7
Sean Kiely <seankiely@Seans-MacBook-Pro-2.local>
2019-06-18 15:27:29 +0100

    added links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+
+
 var slide = document.querySelectorAll('div.about-slide h1');
 var bg = document.querySelector('section.bg-type');
 
@@ -98,8 +101,3 @@ function closeNav() {
 };
 
 
-window.addEventListener('load', (event) => {
-  if (document.referrer.indexOf('work-8.html') > -1) {
-    document.querySelector(".work").click()
-}
-});
